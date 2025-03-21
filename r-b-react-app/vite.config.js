16c05318564525904afd1dc1fb9e6cac83333baa
TRIPLE167 <153914342+TRIPLE167@users.noreply.github.com>
2025-03-22 00:18:11 +0400

    Set up GitHub Pages deploy

--- r-b-react-app/vite.config.js
+++ r-b-react-app/vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Redberry-project/',  
   plugins: [react()],
-  resolve: {
-    alias: {
-      'date-fns': 'date-fns/esm',
-    },
-  },
-  base: '/Redberry-project/', 
-});
-
+})
