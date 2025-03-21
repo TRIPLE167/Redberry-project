fcb7b410576b53fe4d9c33d02525e8b70e4cba83
TRIPLE167 <153914342+TRIPLE167@users.noreply.github.com>
2025-03-21 23:48:02 +0400

    commit

--- r-b-react-app/vite.config.js
+++ r-b-react-app/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
- 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'date-fns': 'date-fns/esm',  
+      'date-fns': 'date-fns/esm',
     },
   },
-})
+  base: '/Redberry-project/', 
+});
+
